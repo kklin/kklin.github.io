13ba7c54347827a17dee77cd6ffe703ce3d3af5b
Kevin Lin <kaikai526@gmail.com>
2015-09-08 21:15:43 -0700

    add fancy scrolling and navbar highlighting

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Scroll spy and scroll filter */
+    $('#main-menu').onePageNav({
+        currentClass: "active",
+        changeHash: false,
+        scrollThreshold: 0.5,
+        scrollSpeed: 750,
+        filter: "",
+        easing: "swing"
+     });
